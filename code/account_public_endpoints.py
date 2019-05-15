baaa1cea3a1696aca9d508d59d7b4d3934d464ef
Ivan Tikhonov <ivan.tikhonov@veeam.com>
2019-05-15 17:22:41 +0300

    dict fix

--- code/account_public_endpoints.py
+++ code/account_public_endpoints.py
@@ -2,10 +2,10 @@ def lambda_handler(event, context):
     print(event)
     print(context)
     response = {
-        statusCode: 200,
-        headers: {
+        "statusCode": 200,
+        "headers": {
           'Content-Type': 'text/html; charset=utf-8'
         },
-        body: '<p>Hello world!</p>'
+        "body": '<p>Hello world!</p>'
         }
     return(response)
