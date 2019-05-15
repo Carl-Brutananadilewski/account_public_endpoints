a328002ed89a208eae582395561262064449782d
Ivan Tikhonov <ivan.tikhonov@veeam.com>
2019-05-15 19:51:46 +0300

    boop2

--- code/account_public_endpoints.py
+++ code/account_public_endpoints.py
@@ -3,10 +3,15 @@ import boto3
 ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
-    response = {}
     response_addresses = []
     addresses = ec2.describe_addresses()
     for eip_dict in addresses['Addresses']:
         response_addresses.append(eip_dict['PublicIp'])
-    response['elastic_ips'] = response_addresses
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": response_addresses
+    }
     return(response)
