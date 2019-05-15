d93359e98a8f14da0ea4c1cad71bc921f833fed2
Ivan Tikhonov <ivan.tikhonov@veeam.com>
2019-05-15 16:41:02 +0300

    return

--- code/account_public_endpoints.py
+++ code/account_public_endpoints.py
@@ -1,4 +1,11 @@
 def lambda_handler(event, context):
-    
     print(event)
     print(context)
+    response = {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'text/html; charset=utf-8'
+        },
+        body: '<p>Hello world!</p>'
+        }
+    return(response)
