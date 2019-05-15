8dfd272209309c6d4e3efc0507b024987bff0b23
Ivan Tikhonov <ivan.tikhonov@veeam.com>
2019-05-15 19:46:55 +0300

    boop

--- code/account_public_endpoints.py
+++ code/account_public_endpoints.py
@@ -3,9 +3,10 @@ import boto3
 ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
-    response = []
+    response = {}
+    response_addresses = []
     addresses = ec2.describe_addresses()
     for eip_dict in addresses['Addresses']:
-        response.append(eip_dict['PublicIp'])
-
+        response_addresses.append(eip_dict['PublicIp'])
+    response['elastic_ips'] = response_addresses
     return(response)
